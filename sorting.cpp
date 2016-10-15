042eb73ba27599cbeb1333aff3f2b3dc575f6da5
cjinlei <cjinlei@umich.edu>
2016-10-14 22:52:57 -0400

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -9,13 +9,13 @@ using namespace std;
 int main()
 {
 	int a[] = {3,34,2,5,30,1,11,22,4,8};
-  //	insertion_sort(a,10);
-  //	shellsort(a,10);
-  //	heapsort(a,10);
-  //	mergesort(a,10);
-	quicksort(a,10);
+  //	insertionAort(a,10);
+  //	shellAort(a,10);
+  //	heapAort(a,10);
+  //	mergeAort(a,10);
+	quickAort(a,10);
 	for (int i=0; i!=10; ++i)
-  cout << a[i] << " ";
+  	cout << a[i] << " ";
 	cout << '\n';
 	return 0;
 }
