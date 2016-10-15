750f2d294d663135a6210ccba4cd923e73459bc9
cjinlei <cjinlei@umich.edu>
2016-10-14 22:39:59 -0400

    Create sorting.cpp

---
+++ sorting.cpp
@@ -0,0 +1,21 @@
+/*
+* test sorting
+*/
+
+#include <iostream>
+#include "sorting.h"
+using namespace std;
+
+int main()
+{
+	int a[] = {3,34,2,5,30,1,11,22,4,8};
+  //	insertion_sort(a,10);
+  //	shellsort(a,10);
+  //	heapsort(a,10);
+  //	mergesort(a,10);
+	quicksort(a,10);
+	for (int i=0; i!=10; ++i)
+  cout << a[i] << " ";
+	cout << '\n';
+	return 0;
+}
